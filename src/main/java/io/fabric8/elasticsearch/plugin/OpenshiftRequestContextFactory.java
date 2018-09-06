jcantrill/openshift-searchguard-sync

bug 1622822. Add uuid to acl docs

src/main/java/io/fabric8/elasticsearch/plugin/OpenshiftRequestContextFactory.java
@@ -38,6 +38,7 @@ import org.elasticsearch.common.logging.Loggers;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.rest.RestRequest;
 
+import io.fabric8.elasticsearch.plugin.OpenshiftAPIService.Project;
 import io.fabric8.elasticsearch.util.RequestUtils;
 
 /**
@@ -80,17 +81,17 @@ public class OpenshiftRequestContextFactory {
     public OpenshiftRequestContext create(final RestRequest request) throws Exception {
         logRequest(request);
 
-        Set<String> projects = new HashSet<>();
+        Set<Project> projects = new HashSet<>();
         boolean isClusterAdmin = false;
         String user = utils.getUser(request);
         String token = utils.getBearerToken(request);
         if (StringUtils.isNotBlank(token)){
             user = utils.assertUser(request);
             isClusterAdmin = utils.isOperationsUser(request);
-            projects = listProjectsFor(user, token);
             if(user.contains("\\")){
                 user = user.replace("\\", "/");
             }
+            projects = listProjectsFor(user, token);
             return new OpenshiftRequestContext(user, token, isClusterAdmin, projects, getKibanaIndex(user, isClusterAdmin), this.kibanaIndexMode);
         }
         LOGGER.debug("Returning EMPTY request context; either was provided client cert or empty token.");
@@ -123,18 +124,18 @@ public class OpenshiftRequestContextFactory {
     // is the only authentication performed in this plugin. This function must
     // throw
     // an exception if the token is invalid.
-    private Set<String> listProjectsFor(final String user, final String token) throws Exception {
+    private Set<Project> listProjectsFor(final String user, final String token) throws Exception {
         final SecurityManager sm = System.getSecurityManager();
         if (sm != null) {
             sm.checkPermission(new SpecialPermission());
         }
-        return AccessController.doPrivileged(new PrivilegedAction<Set<String>>(){
+        return AccessController.doPrivileged(new PrivilegedAction<Set<Project>>(){
 
             @Override
-            public Set<String> run() {
-                Set<String> projects = apiService.projectNames(token);
-                for (Iterator<String> it = projects.iterator(); it.hasNext();) {
-                    if (isBlacklistProject(it.next())) {
+            public Set<Project> run() {
+                Set<Project> projects = apiService.projectNames(token);
+                for (Iterator<Project> it = projects.iterator(); it.hasNext();) {
+                    if (isBlacklistProject(it.next().getName())) {
                         it.remove();
                     }
                 }
@@ -173,17 +174,17 @@ public class OpenshiftRequestContextFactory {
     public static class OpenshiftRequestContext {
 
         public static final OpenshiftRequestContext EMPTY = new OpenshiftRequestContext("", "", false,
-                new HashSet<String>(), "", "");
+                new HashSet<Project>(), "", "");
 
         private final String user;
         private final String token;
         private final boolean isClusterAdmin;
-        private final Set<String> projects;
+        private final Set<Project> projects;
         private final String kibanaIndex;
         private final String kibanaIndexMode;
 
         public OpenshiftRequestContext(final String user, final String token, boolean isClusterAdmin, 
-                Set<String> projects, String kibanaIndex, final String kibanaIndexMode) {
+                Set<Project> projects, String kibanaIndex, final String kibanaIndexMode) {
             this.user = user;
             this.token = token;
             this.isClusterAdmin = isClusterAdmin;
@@ -214,12 +215,11 @@ public class OpenshiftRequestContextFactory {
         }
 
         /**
-         * The Set of projects with UUID
+         * The Set of projects
          * 
-         * @return the set of project names formatted with their UUID (e.g.
-         *         project.UUID)
+         * @return the set of projects
          */
-        public Set<String> getProjects() {
+        public Set<Project> getProjects() {
             return projects;
         }
 

src/main/java/io/fabric8/elasticsearch/plugin/acl/ProjectRolesMappingSyncStrategy.java
@@ -18,6 +18,7 @@ package io.fabric8.elasticsearch.plugin.acl;
 
 import static io.fabric8.elasticsearch.plugin.acl.SearchGuardRoles.PROJECT_PREFIX;
 
+import io.fabric8.elasticsearch.plugin.OpenshiftAPIService.Project;
 import io.fabric8.elasticsearch.plugin.OpenshiftRequestContextFactory.OpenshiftRequestContext;
 
 public class ProjectRolesMappingSyncStrategy extends BaseRolesMappingSyncStrategy {
@@ -30,8 +31,8 @@ public class ProjectRolesMappingSyncStrategy extends BaseRolesMappingSyncStrateg
     protected  void syncFromImpl(OpenshiftRequestContext context, RolesMappingBuilder builder) {
         
         final String username = context.getUser();
-        for (String project : context.getProjects()) {
-            String projectRoleName = String.format("%s_%s", PROJECT_PREFIX, project.replace('.', '_'));
+        for (Project project : context.getProjects()) {
+            String projectRoleName = String.format("%s_%s", PROJECT_PREFIX, project.getName().replace('.', '_'));
 
             builder.addUser(projectRoleName, username);
         }

src/main/java/io/fabric8/elasticsearch/plugin/acl/UserRolesSyncStrategy.java
@@ -18,6 +18,7 @@ package io.fabric8.elasticsearch.plugin.acl;
 
 import org.apache.commons.lang.StringUtils;
 
+import io.fabric8.elasticsearch.plugin.OpenshiftAPIService.Project;
 import io.fabric8.elasticsearch.plugin.OpenshiftRequestContextFactory.OpenshiftRequestContext;
 
 public class UserRolesSyncStrategy extends BaseRolesSyncStrategy implements RolesSyncStrategy {
@@ -58,14 +59,14 @@ public class UserRolesSyncStrategy extends BaseRolesSyncStrategy implements Role
                 .expires(expire);
 
         //permissions for projects
-        for (String project : context.getProjects()) {
-            String indexName = String.format("%s?*", project.replace('.', '?'));
+        for (Project project : context.getProjects()) {
+            String indexName = String.format("%s?%s?*", project.getName().replace('.', '?'), project.getUID());
             role.setActions(indexName, ALL, PROJECT_ROLE_ACTIONS);
             // If using common data model, allow access to both the
             // $projname.$uuid.* indices and
             // the project.$projname.$uuid.* indices for backwards compatibility
             if (StringUtils.isNotEmpty(cdmProjectPrefix)) {
-                indexName = String.format("%s?%s?*", cdmProjectPrefix.replace('.', '?'), project.replace('.', '?'));
+                indexName = String.format("%s?%s?%s?*", cdmProjectPrefix.replace('.', '?'), project.getName().replace('.', '?'), project.getUID());
                 role.setActions(indexName, ALL, PROJECT_ROLE_ACTIONS);
             }
         }

src/test/java/io/fabric8/elasticsearch/plugin/acl/SearchGuardRoleACLTest.java
@@ -22,8 +22,9 @@ import static org.junit.Assert.assertEquals;
 
 import java.io.StringReader;
 import java.util.Arrays;
-import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.elasticsearch.common.bytes.BytesArray;
 import org.elasticsearch.common.xcontent.XContentHelper;
@@ -31,6 +32,7 @@ import org.elasticsearch.common.xcontent.XContentType;
 import org.junit.Test;
 
 import io.fabric8.elasticsearch.plugin.KibanaIndexMode;
+import io.fabric8.elasticsearch.plugin.OpenshiftAPIService.Project;
 import io.fabric8.elasticsearch.plugin.OpenshiftRequestContextFactory.OpenshiftRequestContext;
 import io.fabric8.elasticsearch.plugin.Samples;
 import io.fabric8.elasticsearch.plugin.acl.SearchGuardRoles.Roles;
@@ -39,8 +41,12 @@ public class SearchGuardRoleACLTest {
 
     private SearchGuardRoles roles = new SearchGuardRoles();
     
+    private Set<Project> createProjects(String...names){
+        return Arrays.asList(names).stream().map(p -> new Project(p,"123abc")).collect(Collectors.toSet());
+    }
+    
     private OpenshiftRequestContext givenContextFor(String user, boolean isOperations, String mode, String...projects) {
-        return new OpenshiftRequestContext(user, "", isOperations, new HashSet<>(Arrays.asList(projects)), "someKibanaIndexValue", mode);
+        return new OpenshiftRequestContext(user, "", isOperations, createProjects(projects), "someKibanaIndexValue", mode);
     }
     
     private ProjectRolesSyncStrategy givenProjectRolesSyncStrategyFor(String mode) {

src/test/java/io/fabric8/elasticsearch/plugin/acl/SearchGuardRolesMappingACLTest.java
@@ -22,9 +22,10 @@ import static org.junit.Assert.assertEquals;
 
 import java.io.StringReader;
 import java.util.Arrays;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.elasticsearch.common.bytes.BytesArray;
 import org.elasticsearch.common.xcontent.XContentHelper;
@@ -32,6 +33,7 @@ import org.elasticsearch.common.xcontent.XContentType;
 import org.junit.Test;
 
 import io.fabric8.elasticsearch.plugin.KibanaIndexMode;
+import io.fabric8.elasticsearch.plugin.OpenshiftAPIService.Project;
 import io.fabric8.elasticsearch.plugin.OpenshiftRequestContextFactory.OpenshiftRequestContext;
 import io.fabric8.elasticsearch.plugin.Samples;
 import io.fabric8.elasticsearch.plugin.acl.SearchGuardRolesMapping.RolesMapping;
@@ -40,9 +42,12 @@ public class SearchGuardRolesMappingACLTest {
 
     private SearchGuardRolesMapping rolesMapping = new SearchGuardRolesMapping();
     
+    private Set<Project> createProjects(String...names){
+        return Arrays.asList(names).stream().map(p -> new Project(p,"123abc")).collect(Collectors.toSet());
+    }
     
     private OpenshiftRequestContext givenContextFor(String user, String kibIndexMode, boolean isOperations, String...projects) {
-        return new OpenshiftRequestContext(user, "", isOperations, new HashSet<>(Arrays.asList(projects)), "abc", kibIndexMode);
+        return new OpenshiftRequestContext(user, "", isOperations, createProjects(projects), "abc", kibIndexMode);
     }
     
     private ProjectRolesMappingSyncStrategy givenProjectRolesSyncStrategy() {

fabric8io/openshift-elasticsearch-plugin

src/main/java/io/fabric8/elasticsearch/plugin/OpenshiftRequestContextFactory.java
@@ -38,6 +38,7 @@ import org.elasticsearch.common.logging.Loggers;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.rest.RestRequest;
 
+import io.fabric8.elasticsearch.plugin.model.Project;
 import io.fabric8.elasticsearch.util.RequestUtils;
 
 /**
@@ -80,17 +81,17 @@ public class OpenshiftRequestContextFactory {
     public OpenshiftRequestContext create(final RestRequest request) throws Exception {
         logRequest(request);
 
-        Set<String> projects = new HashSet<>();
+        Set<Project> projects = new HashSet<>();
         boolean isClusterAdmin = false;
         String user = utils.getUser(request);
         String token = utils.getBearerToken(request);
         if (StringUtils.isNotBlank(token)){
             user = utils.assertUser(request);
             isClusterAdmin = utils.isOperationsUser(request);
-            projects = listProjectsFor(user, token);
             if(user.contains("\\")){
                 user = user.replace("\\", "/");
             }
+            projects = listProjectsFor(user, token);
             return new OpenshiftRequestContext(user, token, isClusterAdmin, projects, getKibanaIndex(user, isClusterAdmin), this.kibanaIndexMode);
         }
         LOGGER.debug("Returning EMPTY request context; either was provided client cert or empty token.");
@@ -123,18 +124,18 @@ public class OpenshiftRequestContextFactory {
     // is the only authentication performed in this plugin. This function must
     // throw
     // an exception if the token is invalid.
-    private Set<String> listProjectsFor(final String user, final String token) throws Exception {
+    private Set<Project> listProjectsFor(final String user, final String token) throws Exception {
         final SecurityManager sm = System.getSecurityManager();
         if (sm != null) {
             sm.checkPermission(new SpecialPermission());
         }
-        return AccessController.doPrivileged(new PrivilegedAction<Set<String>>(){
+        return AccessController.doPrivileged(new PrivilegedAction<Set<Project>>(){
 
             @Override
-            public Set<String> run() {
-                Set<String> projects = apiService.projectNames(token);
-                for (Iterator<String> it = projects.iterator(); it.hasNext();) {
-                    if (isBlacklistProject(it.next())) {
+            public Set<Project> run() {
+                Set<Project> projects = apiService.projectNames(token);
+                for (Iterator<Project> it = projects.iterator(); it.hasNext();) {
+                    if (isBlacklistProject(it.next().getName())) {
                         it.remove();
                     }
                 }
@@ -173,17 +174,17 @@ public class OpenshiftRequestContextFactory {
     public static class OpenshiftRequestContext {
 
         public static final OpenshiftRequestContext EMPTY = new OpenshiftRequestContext("", "", false,
-                new HashSet<String>(), "", "");
+                new HashSet<Project>(), "", "");
 
         private final String user;
         private final String token;
         private final boolean isClusterAdmin;
-        private final Set<String> projects;
+        private final Set<Project> projects;
         private final String kibanaIndex;
         private final String kibanaIndexMode;
 
         public OpenshiftRequestContext(final String user, final String token, boolean isClusterAdmin, 
-                Set<String> projects, String kibanaIndex, final String kibanaIndexMode) {
+                Set<Project> projects, String kibanaIndex, final String kibanaIndexMode) {
             this.user = user;
             this.token = token;
             this.isClusterAdmin = isClusterAdmin;
@@ -214,12 +215,11 @@ public class OpenshiftRequestContextFactory {
         }
 
         /**
-         * The Set of projects with UUID
+         * The Set of projects
          * 
-         * @return the set of project names formatted with their UUID (e.g.
-         *         project.UUID)
+         * @return the set of projects
          */
-        public Set<String> getProjects() {
+        public Set<Project> getProjects() {
             return projects;
         }
 

src/main/java/io/fabric8/elasticsearch/plugin/acl/ProjectRolesMappingSyncStrategy.java
@@ -19,6 +19,7 @@ package io.fabric8.elasticsearch.plugin.acl;
 import static io.fabric8.elasticsearch.plugin.acl.SearchGuardRoles.PROJECT_PREFIX;
 
 import io.fabric8.elasticsearch.plugin.OpenshiftRequestContextFactory.OpenshiftRequestContext;
+import io.fabric8.elasticsearch.plugin.model.Project;
 
 public class ProjectRolesMappingSyncStrategy extends BaseRolesMappingSyncStrategy {
 
@@ -30,8 +31,8 @@ public class ProjectRolesMappingSyncStrategy extends BaseRolesMappingSyncStrateg
     protected  void syncFromImpl(OpenshiftRequestContext context, RolesMappingBuilder builder) {
         
         final String username = context.getUser();
-        for (String project : context.getProjects()) {
-            String projectRoleName = String.format("%s_%s", PROJECT_PREFIX, project.replace('.', '_'));
+        for (Project project : context.getProjects()) {
+            String projectRoleName = String.format("%s_%s", PROJECT_PREFIX, project.getName().replace('.', '_'));
 
             builder.addUser(projectRoleName, username);
         }

src/main/java/io/fabric8/elasticsearch/plugin/acl/UserRolesSyncStrategy.java
@@ -19,6 +19,7 @@ package io.fabric8.elasticsearch.plugin.acl;
 import org.apache.commons.lang.StringUtils;
 
 import io.fabric8.elasticsearch.plugin.OpenshiftRequestContextFactory.OpenshiftRequestContext;
+import io.fabric8.elasticsearch.plugin.model.Project;
 
 public class UserRolesSyncStrategy extends BaseRolesSyncStrategy implements RolesSyncStrategy {
 
@@ -58,14 +59,14 @@ public class UserRolesSyncStrategy extends BaseRolesSyncStrategy implements Role
                 .expires(expire);
 
         //permissions for projects
-        for (String project : context.getProjects()) {
-            String indexName = String.format("%s?*", project.replace('.', '?'));
+        for (Project project : context.getProjects()) {
+            String indexName = String.format("%s?%s?*", project.getName().replace('.', '?'), project.getUID());
             role.setActions(indexName, ALL, PROJECT_ROLE_ACTIONS);
             // If using common data model, allow access to both the
             // $projname.$uuid.* indices and
             // the project.$projname.$uuid.* indices for backwards compatibility
             if (StringUtils.isNotEmpty(cdmProjectPrefix)) {
-                indexName = String.format("%s?%s?*", cdmProjectPrefix.replace('.', '?'), project.replace('.', '?'));
+                indexName = String.format("%s?%s?%s?*", cdmProjectPrefix.replace('.', '?'), project.getName().replace('.', '?'), project.getUID());
                 role.setActions(indexName, ALL, PROJECT_ROLE_ACTIONS);
             }
         }

src/test/java/io/fabric8/elasticsearch/plugin/acl/SearchGuardRoleACLTest.java
@@ -22,8 +22,9 @@ import static org.junit.Assert.assertEquals;
 
 import java.io.StringReader;
 import java.util.Arrays;
-import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.elasticsearch.common.bytes.BytesArray;
 import org.elasticsearch.common.xcontent.XContentHelper;
@@ -34,13 +35,18 @@ import io.fabric8.elasticsearch.plugin.KibanaIndexMode;
 import io.fabric8.elasticsearch.plugin.OpenshiftRequestContextFactory.OpenshiftRequestContext;
 import io.fabric8.elasticsearch.plugin.Samples;
 import io.fabric8.elasticsearch.plugin.acl.SearchGuardRoles.Roles;
+import io.fabric8.elasticsearch.plugin.model.Project;
 
 public class SearchGuardRoleACLTest {
 
     private SearchGuardRoles roles = new SearchGuardRoles();
     
+    private Set<Project> createProjects(String...names){
+        return Arrays.asList(names).stream().map(p -> new Project(p,"123abc")).collect(Collectors.toSet());
+    }
+    
     private OpenshiftRequestContext givenContextFor(String user, boolean isOperations, String mode, String...projects) {
-        return new OpenshiftRequestContext(user, "", isOperations, new HashSet<>(Arrays.asList(projects)), "someKibanaIndexValue", mode);
+        return new OpenshiftRequestContext(user, "", isOperations, createProjects(projects), "someKibanaIndexValue", mode);
     }
     
     private ProjectRolesSyncStrategy givenProjectRolesSyncStrategyFor(String mode) {

src/test/java/io/fabric8/elasticsearch/plugin/acl/SearchGuardRolesMappingACLTest.java
@@ -22,9 +22,10 @@ import static org.junit.Assert.assertEquals;
 
 import java.io.StringReader;
 import java.util.Arrays;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.elasticsearch.common.bytes.BytesArray;
 import org.elasticsearch.common.xcontent.XContentHelper;
@@ -35,14 +36,18 @@ import io.fabric8.elasticsearch.plugin.KibanaIndexMode;
 import io.fabric8.elasticsearch.plugin.OpenshiftRequestContextFactory.OpenshiftRequestContext;
 import io.fabric8.elasticsearch.plugin.Samples;
 import io.fabric8.elasticsearch.plugin.acl.SearchGuardRolesMapping.RolesMapping;
+import io.fabric8.elasticsearch.plugin.model.Project;
 
 public class SearchGuardRolesMappingACLTest {
 
     private SearchGuardRolesMapping rolesMapping = new SearchGuardRolesMapping();
     
+    private Set<Project> createProjects(String...names){
+        return Arrays.asList(names).stream().map(p -> new Project(p,"123abc")).collect(Collectors.toSet());
+    }
     
     private OpenshiftRequestContext givenContextFor(String user, String kibIndexMode, boolean isOperations, String...projects) {
-        return new OpenshiftRequestContext(user, "", isOperations, new HashSet<>(Arrays.asList(projects)), "abc", kibIndexMode);
+        return new OpenshiftRequestContext(user, "", isOperations, createProjects(projects), "abc", kibIndexMode);
     }
     
     private ProjectRolesMappingSyncStrategy givenProjectRolesSyncStrategy() {